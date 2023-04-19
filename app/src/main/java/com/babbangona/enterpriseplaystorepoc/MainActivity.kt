d25bd7022c9bb48c22028400f981c015a36a9774
Rehoboth <rehoboth.iyasele@babbangona.com>
2023-04-19 18:33:34 +0100

    Beta testing branch

--- app/src/main/java/com/babbangona/enterpriseplaystorepoc/MainActivity.kt
+++ app/src/main/java/com/babbangona/enterpriseplaystorepoc/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.babbangona.enterpriseplaystorepoc
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.tvH2)
+        tv.text = "This is the beta-testing branch versionName: ${BuildConfig.VERSION_NAME}"
     }
 }
