4c516888b4b3965c0c338fa90860389ea8a6c050
Rehoboth <rehoboth.iyasele@babbangona.com>
2023-04-27 11:12:57 +0100

    Closed testing build is now uploaded, but the production users are also getting the update.

--- app/src/main/java/com/babbangona/enterpriseplaystorepoc/MainActivity.kt
+++ app/src/main/java/com/babbangona/enterpriseplaystorepoc/MainActivity.kt
@@ -7,11 +7,13 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tv: TextView
+    private lateinit var tV2: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tv = findViewById(R.id.tvH2)
+        tV2 = findViewById(R.id.textView)
         tv.text = "This is the closed-testing branch versionName: ${BuildConfig.VERSION_NAME}"
     }
 }
